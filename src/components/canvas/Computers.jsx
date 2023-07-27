02461daf3447b615d30d61fdab6dae268e147e36
root <root@LAPTOP-5RAL3233>
2023-07-27 12:52:28 +0100

    added the scrol to about

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -36,6 +36,8 @@ const ComputersCanvas = () => {
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+
     >
       {/* This will prevent you from rotating the visual around completely */}
       <Suspense fallback={<CanvasLoader />}>
