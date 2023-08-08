9e19b8b38388654f348245809205fd64d16a58d0
root <root@LAPTOP-5RAL3233>
2023-08-08 22:28:05 +0100

    Just need to host!

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import TCS from "./company/TCS.png";
+import baker from "./company/BAKER-HUGHES.jpeg";
+import mechan from "./company/MECHAN-CONTROLS-PLC.png";
+import uu from "./company/UNITED-UTILITIES.jpeg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,11 +52,12 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  TCS,
+  baker,
+  mechan,
+  uu,
   carrent,
   jobit,
   tripguide,
 };
+
