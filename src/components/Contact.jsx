04efb728c155f3eb66343b7c32d218b0c0bd1205
root <root@LAPTOP-5RAL3233>
2023-08-20 11:50:02 +0100

    Added email function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//ZvWJLftlQGa4655AH
+//template_ruk43y5
+//service_gxj4y0g
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -30,20 +34,22 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
+// ZvWJLftlQGa4655AH
+//template_ruk43y5
+// service_gxj4y0g
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_gxj4y0g',
+        'template_ruk43y5',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Mohamed",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "swayd.mohamed@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
+        'ZvWJLftlQGa4655AH'
+        )
       .then(
         () => {
           setLoading(false);
